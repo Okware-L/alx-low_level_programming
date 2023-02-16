d0ac04c798a78772e274fc59ecccc2405da8b1ae
Okware-L <lewisokware@gmail.com>
2023-02-16 07:29:20 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - prints without printf and puts
+ *Return: 1
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+return (1);
+}
