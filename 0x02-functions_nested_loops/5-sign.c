ace14274394a0563b808e8acaf29643e46102b6a
Okware-L <lewisokware@gmail.com>
2023-02-21 05:39:33 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *print_sign - check the numbers
+ *@n : number being checked
+ *Return: 0 or 1
+ */
+int print_sign(int n)
+{
+int test;
+
+if (n > 0)
+{
+	test = 1;
+	_putchar('+');
+}
+else if (n == 0)
+{
+	test = 0;
+	putchar('0');
+}
+else
+{
+	test = -1;
+	putchar('-');
+}
+return (test);
+}
