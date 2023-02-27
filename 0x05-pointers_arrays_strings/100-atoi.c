91968ad7ece5a4522e8b4caf309a1462da89e9ce
Okware-L <lewisokware@gmail.com>
2023-02-27 01:04:27 -0800

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _atoi - convert string to an int
+ * @s: input
+ * Return: conversion
+ */
+int _atoi(char *s)
+{
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *_strcopy - copy string pointed to
+ * @dest: destination
+ * @src: source
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i]
+	}
+	return (dest);
+}
